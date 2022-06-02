d96b25c6b1a0c03a513d25327f4825f0e5212b2e
Lars Dedman <105458848+DedmanL@users.noreply.github.com>
2022-06-02 13:26:38 -0600

    Update Customer.cs

--- CKK.Logic/Models/Customer.cs
+++ CKK.Logic/Models/Customer.cs
@@ -12,11 +12,11 @@ namespace CKK.Logic.Models
         private string _name;
         private string _address;
 
-        public int GetID()
+        public int GetId()
         {
             return _id;
         }
-        public void SetID(int id)
+        public void SetId(int id)
         {
             _id = id;
         }
