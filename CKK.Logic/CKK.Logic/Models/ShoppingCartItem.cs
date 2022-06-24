c8bed31ade3d29f7ec280104ad49ef6d14a8421c
Lars Dedman <105458848+DedmanL@users.noreply.github.com>
2022-06-24 10:49:16 -0600

    Add files via upload

---
+++ CKK.Logic/CKK.Logic/Models/ShoppingCartItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    public class ShoppingCartItem
+    {
+        private Product _product;
+        private int _quantity;
+        public ShoppingCartItem(Product prod, int quantity)
+        {
+            _product = prod;
+            _quantity = quantity;
+        }
+        public int GetQuantity()
+        {
+            return _quantity;
+        }
+        public void SetQuantity(int quantity)
+        {
+            _quantity = quantity;
+        }
+        public Product GetProduct()
+        {
+            return _product;
+        }
+        public void SetProduct(Product prod)
+        {
+            _product = prod;
+        }
+        public decimal GetTotal()//Method that multiplies the price and quantity to get a total cost.
+        {
+            decimal total = _product._price * _quantity;
+            return total;
+        }
+    }
+}
