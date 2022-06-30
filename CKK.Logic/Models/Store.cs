dd2bebd42b87a3ecd5bc02ce0a94e73234def725
Lars Dedman <105458848+DedmanL@users.noreply.github.com>
2022-06-30 14:22:30 -0600

    Add files via upload

--- CKK.Logic/Models/Store.cs
+++ CKK.Logic/Models/Store.cs
@@ -34,7 +34,7 @@ namespace CKK.Logic.Models
         }
         public int RemoveStoreItem(int productNumber)//Method that removes a product from the store.
         {
-            if(productNumber >= 1)//States value must be greater than or equal to 1 to execute next line.
+            while(productNumber >= 1)//States value must be greater than or equal to 1 to execute next line.
             { productNumber -= 1; }//Change this.
             if(productNumber <= 0)//States value must be less than or equal to 0 to execute next line.
             { productNumber = 0; }//Sets value at 0 based on statement.
@@ -43,10 +43,10 @@ namespace CKK.Logic.Models
         }
         public int GetStoreItem(int productNumber)//Method that gets the product by its position.
         {
-            if ()//get method for product based on position.
-            {  }
-            if (productNumber != )//invalid product number.
-            {  }
+            if (productNumber == 1)//get method for product based on position.
+            { productNumber = prod; }
+            if (productNumber != (product1, product2, product3) )//invalid product number.
+            { prod = null; }
             if (product1 == null)//P1 is empty return null.
             { prod = null; }
             if (product2 == null)//P2 is empty return null.
@@ -57,10 +57,10 @@ namespace CKK.Logic.Models
         }
         public int FindStoreItemByld(int id)
         {
-            if (id == product1._id) { return product1._id; }
-                else if (id != product1._id) { return product2._id; }
-                    else if (id != product2._id) { return product3._id; }
-                        else if (id != product3._id) { return null; }//return null.
+            if (id == product1._id) { prod = product1; }
+                else if (id != product1._id) { prod = product2; }
+                    else if (id != product2._id) { prod = product3; }
+                        else if (id != product3._id) { prod = null; }//return null.
 
             if (product1._id == product2._id) { id = product1._id; }
                 else if (product2._id == product3._id) { id = product2._id; }
